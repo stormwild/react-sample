2f78d3d50062e40af0b65e6740c1ff4cda1075a0
Alexander R. Torrijos <artorrijos@gmail.com>
2016-12-11 06:04:54 +0000

    Rows generated from data

--- activities/app.js
+++ activities/app.js
@@ -1,12 +1,31 @@
 var App = React.createClass({
-    render: function(){
+    render: function() {
+        
+        var data = [{ 
+            "when": "2 minutes ago", 
+            "who": "Jill Dupre", 
+            "description": "Created new account"
+        },
+        {
+            "when": "1 hour ago",
+            "who": "Lose White",
+            "description": "Added fist chapter"
+        },
+        {
+            "when": "2 hours ago",
+            "who": "Jordan Whash",
+            "description": "Created new account"
+        }];
+
+        var rows = data.map(function(row) {
+            return <tr><td>{ row.when }</td><td>{ row.who }</td><td>{ row.description }</td></tr>
+        });
+        
         return <div>
             <h1>Recent Changes</h1>
             <table>
-                <thead><th>When</th><th>Who</th><th>Description</th></thead>
-                <tr><td>2 minutes ago</td><td>Jill Dupre</td><td>Created new account</td></tr>
-                <tr><td>1 hour ago</td><td>Lose White</td><td>Added fist chapter</td></tr>
-                <tr><td>2 hours ago</td><td>Jordan Whash</td><td>Created new account</td></tr>
+                <thead><tr><th>When</th><th>Who</th><th>Description</th></tr></thead>
+                <tbody>{ rows }</tbody>
             </table>
         </div>;
     }
