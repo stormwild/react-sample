4d4407194af2c69dc9ff618a3f1c2f5dc60fb091
Alexander R. Torrijos <artorrijos@gmail.com>
2016-12-11 05:51:56 +0000

    Added heading

--- activities/app.js
+++ activities/app.js
@@ -1,11 +1,14 @@
 var App = React.createClass({
     render: function(){
-        return <table>
-            <thead><th>When</th><th>Who</th><th>Description</th></thead>
-            <tr><td>2 minutes ago</td><td>Jill Dupre</td><td>Created new account</td></tr>
-            <tr><td>1 hour ago</td><td>Lose White</td><td>Added fist chapter</td></tr>
-            <tr><td>2 hours ago</td><td>Jordan Whash</td><td>Created new account</td></tr>
-        </table>;
+        return <div>
+            <h1>Recent Changes</h1>
+            <table>
+                <thead><th>When</th><th>Who</th><th>Description</th></thead>
+                <tr><td>2 minutes ago</td><td>Jill Dupre</td><td>Created new account</td></tr>
+                <tr><td>1 hour ago</td><td>Lose White</td><td>Added fist chapter</td></tr>
+                <tr><td>2 hours ago</td><td>Jordan Whash</td><td>Created new account</td></tr>
+            </table>
+        </div>;
     }
 });
 
