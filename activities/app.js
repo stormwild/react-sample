7d06b320e664439bf9c935b7ab8bc7434731c3cf
Alexander R. Torrijos <artorrijos@gmail.com>
2016-12-11 05:48:50 +0000

    Added table data

--- activities/app.js
+++ activities/app.js
@@ -1,6 +1,11 @@
 var App = React.createClass({
     render: function(){
-        return <p>Welcome to React!</p>;
+        return <table>
+            <thead><th>When</th><th>Who</th><th>Description</th></thead>
+            <tr><td>2 minutes ago</td><td>Jill Dupre</td><td>Created new account</td></tr>
+            <tr><td>1 hour ago</td><td>Lose White</td><td>Added fist chapter</td></tr>
+            <tr><td>2 hours ago</td><td>Jordan Whash</td><td>Created new account</td></tr>
+        </table>;
     }
 });
 
