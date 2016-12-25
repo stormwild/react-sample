02a2cd553022970fd01eac48c00fb058e401c3d1
Alexander R. Torrijos <artorrijos@gmail.com>
2016-12-25 06:44:05 +0000

    Added next task

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BookStore, BookList, ShippingDetails, DeliveryDetails } from './BookStore';
 
+// @TODO Shipping Details
 ReactDOM.render(<BookStore />, document.getElementById('root'));
