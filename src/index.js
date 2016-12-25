45ec3e9dfa2a69cfeb3b8a97559c73162c24541d
Alexander R. Torrijos <artorrijos@gmail.com>
2016-12-25 05:13:32 +0000

    InputExample as Controlled component

--- src/index.js
+++ src/index.js
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom';
 import BookStore from './BookStore';
 
 var InputExample = React.createClass({
+    getInitialState() {
+        return ({ name: '-' });
+    },
+    
+    handleChangeEvent(event) {
+        this.setState({ name: event.target.value.toUpperCase() });
+    },
+    
     render() {
-        return <input type="text" value="Hello" />;
+        return <input type="text" value={ this.state.name } onChange={ this.handleChangeEvent } />;
     }
 });
 
